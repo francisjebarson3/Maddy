188b08371b63b310ee14ada418038a2116526e0a
francisjebarson3 <40454851+francisjebarson3@users.noreply.github.com>
2018-06-21 14:15:47 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Positive
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		if(s>0)
+		System.out.println("Positive");
+		else 
+		System.out.println("Zero");
+	}
+}
