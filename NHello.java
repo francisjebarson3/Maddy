97a3a0a002650b65bba4df87bfd8107a28f4733a
francisjebarson3 <40454851+francisjebarson3@users.noreply.github.com>
2018-06-27 09:54:04 +0530

    Create NHello.java

---
+++ NHello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class NHello
+{
+    public static void main(String fran[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+       for(int i=0;i<N;i++)
+       System.out.println("Hello");
+    }
+}
