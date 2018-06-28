e3f72e631b5bd87295c5708bdcda6de1a7b3f5a9
francisjebarson3 <40454851+francisjebarson3@users.noreply.github.com>
2018-06-28 14:05:01 +0530

    Create Pow.java

---
+++ Pow.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Pow
+{
+    public static void main(String frn[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int P=0;
+        int po=sc.nextInt();
+        System.out.print((int)Math.pow(N,po));
+    }
+    
+}
